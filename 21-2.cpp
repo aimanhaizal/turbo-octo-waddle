53eb1e982f57857eb03d52e2b1684c63f73af0f1
PAYEONGWAON <78063595+PAYEONGWAON@users.noreply.github.com>
2021-02-22 12:19:33 +0800

    Update 21-2.cpp

--- 21-2.cpp
+++ 21-2.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <iostream> 
+#include <iomanip> 
+#include <cstdlib> 
+#include <string> 
+#include <vector> 
+#include <ctime> 
+#include <windows.h>
 using namespace std;
-#include <vector>
 
 class Map {
     private:
@@ -20,6 +25,33 @@ class Map {
             }
         }
 };
+
+void Mars::init()
+{
+    char objects[] = {' ', ' ', ' ', ' ', ' ', ' ',
+    'X', '#', '@', '$' };
+    int noOfObjects = 10;	//number of objects in the objects array
+
+    dimX = 15;
+    dimY = 5;
+
+    //create dynamic 2D array using vector 
+    map.resize(dimY);	//create rows 
+    for (int i=0; i<dimY; ++i)
+    {
+    map[i].resize(dimX);	//resize each rows
+    }
+
+    //put random chars into the vector array 
+    for (int i=0; i<dimY; ++i)
+    {
+    for (int j=0; j<dimX; ++j)
+        {
+        int objNo = rand() % noOfObjects; map[i][j] =	objects[ objNo ];
+        }
+    }
+}
+
 
 int main(){
     Map a(2,3);
